3cce42ca6d0c110a36443778b6cff6f81a1cfef4
Smin <sminnims1004@naver.com>
2026-03-24 15:16:29 +0900

    Import TMP / Map Noise Change Test

--- Assets/Script/FlyweightPattern.cs
+++ Assets/Script/FlyweightPattern.cs
@@ -4,6 +4,7 @@ using Unity.Mathematics; // 패키지 매니저에서 Mathematics 설치 필요
 public class FlyweightPattern : MonoBehaviour
 {
     TileData[,] testData;
+    SpriteRenderer[,] tileObjects;
     [SerializeField] private GameObject testWater; // 타일 프리팹
     [SerializeField] private GameObject testSand; // 타일 프리팹
 
@@ -11,13 +12,40 @@ public class FlyweightPattern : MonoBehaviour
 
     void Start()
     {
-        for(int i = 0; i < size.x; i++)
-        {
-            for(int j = 0; j < size.y; j++)
+        tileObjects = new SpriteRenderer[chunkSize * size.x, chunkSize * size.y];
+
+        for (int i = 0; i < size.x; i++)
             {
-                testData = GenerateChunkData(new Vector2Int(i, j));
+                for (int j = 0; j < size.y; j++)
+                {
+                    testData = GenerateChunkData(new Vector2Int(i, j));
+                }
             }
-        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < size.x; i++)
+            {
+                for (int j = 0; j < size.y; j++)
+                {
+                    ReColor(new Vector2Int(i, j));
+                }
+            }
+
+        if(Input.GetKeyDown(KeyCode.Q)) noiseScale += 10f;
+        if(Input.GetKeyDown(KeyCode.W)) noiseScale -= 10f;
+
+        if(Input.GetKeyDown(KeyCode.E)) noiseOctaves += 1;
+        if(Input.GetKeyDown(KeyCode.R)) noiseOctaves -= 1;
+
+        if(Input.GetKeyDown(KeyCode.T)) noisePersistence += 0.1f;
+        if(Input.GetKeyDown(KeyCode.Y)) noisePersistence -= 0.1f;
+
+        if(Input.GetKeyDown(KeyCode.U)) noiseLacunarity += 0.5f;
+        if(Input.GetKeyDown(KeyCode.I)) noiseLacunarity -= 0.5f;
+
+        if(Input.GetKeyDown(KeyCode.Space)) mapSeed = UnityEngine.Random.Range(0f, 99999f);
     }
 
     public float noiseScale = 50f;
@@ -27,6 +55,37 @@ public class FlyweightPattern : MonoBehaviour
     public float mapSeed = 12345f; // 랜덤 시드
 
     public int chunkSize = 16;
+
+    void ReColor(Vector2Int chunkCoordinate)
+    {
+        float startWorldX = chunkCoordinate.x * chunkSize;
+        float startWorldY = chunkCoordinate.y * chunkSize;
+
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                float worldX = startWorldX + x;
+                float worldY = startWorldY + y;
+
+                float currentHeight = NoiseGenerator.GenerateFbmNoise
+                (worldX, worldY, noiseScale, noiseOctaves, noisePersistence, noiseLacunarity, mapSeed);
+
+                // 노이즈 값에 따라 지형 결정
+                TerrainType currentTerrain = DetermineTerrainType(currentHeight);
+
+                tileObjects[(int)worldX, (int)worldY].color = currentTerrain switch
+                {
+                    TerrainType.DeepWater => Color.blue,
+                    TerrainType.ShallowWater => Color.cyan,
+                    TerrainType.Sand => Color.yellow,
+                    TerrainType.Grass => Color.gray,
+                    TerrainType.Forest => Color.green,
+                    _ => throw new System.NotImplementedException()
+                };
+            }
+        }
+    }
 
     // 특정 청크 좌표(예: 0,0 또는 1,0)에 대한 타일 데이터를 생성하여 반환
     public TileData[,] GenerateChunkData(Vector2Int chunkCoordinate)
@@ -53,22 +112,28 @@ public class FlyweightPattern : MonoBehaviour
                 TerrainType currentTerrain = DetermineTerrainType(currentHeight);
 
                 // 타일 데이터 저장
-                chunkTiles[x, y] = new TileData 
-                { 
-                    terrain = currentTerrain, 
+                chunkTiles[x, y] = new TileData
+                {
+                    terrain = currentTerrain,
                     heightValue = currentHeight
                 };
                 //Debug.Log($"Tile at ({x}, {y}): {currentTerrain}, Height: {currentHeight} / World Pos: ({worldX}, {worldY})");
-                GameObject tilePrefab = currentTerrain switch
+                    
+                if (tileObjects[(int)worldX, (int)worldY] == null)
                 {
-                    TerrainType.DeepWater => testWater,
-                    TerrainType.ShallowWater => testWater,
-                    TerrainType.Sand => testSand,
-                    TerrainType.Grass => testSand,
-                    TerrainType.Forest => testSand,
+                    tileObjects[(int)worldX, (int)worldY] = Instantiate(testSand, new Vector3(worldX, worldY, 0), Quaternion.identity, this.transform).GetComponent<SpriteRenderer>();
+                }
+
+                tileObjects[(int)worldX, (int)worldY].color = currentTerrain switch
+                {
+                    TerrainType.DeepWater => Color.blue,
+                    TerrainType.ShallowWater => Color.cyan,
+                    TerrainType.Sand => Color.yellow,
+                    TerrainType.Grass => Color.gray,
+                    TerrainType.Forest => Color.green,
                     _ => throw new System.NotImplementedException()
                 };
-                Instantiate(tilePrefab, new Vector3(worldX, worldY, 0), Quaternion.identity, this.transform);
+
             }
         }
 
@@ -87,13 +152,13 @@ public class FlyweightPattern : MonoBehaviour
 }
 
 // 타일의 종류를 정의
-public enum TerrainType 
-{ 
-    DeepWater, 
-    ShallowWater, 
-    Sand, 
-    Grass, 
-    Forest 
+public enum TerrainType
+{
+    DeepWater,
+    ShallowWater,
+    Sand,
+    Grass,
+    Forest
 }
 
 // 가벼운 데이터 구조체 (Flyweight)
@@ -101,6 +166,7 @@ public struct TileData
 {
     public TerrainType terrain;
     public float heightValue; // 노이즈 결과값 (디버깅용 또는 부가 로직용)
+    public GameObject testObj;
     // 향후 oreType (자원 종류) 등이 추가될 수 있음
 }
 
@@ -108,7 +174,7 @@ public static class NoiseGenerator
 {
     // FBM 기반 노이즈 생성 함수
     public static float GenerateFbmNoise(
-        float xPosition, float yPosition, float scale, int octaves, 
+        float xPosition, float yPosition, float scale, int octaves,
         float persistence, float lacunarity, float seedOffset)
     {
         if (scale <= 0) scale = 0.0001f;
@@ -137,7 +203,7 @@ public static class NoiseGenerator
 
         // -1 ~ 1 형태의 결과를 0 ~ 1 사이로 정규화하여 반환
         float normalizedNoise = (totalNoise / maxValue + 1f) / 2f;
-        
+
         // 간혹 범위를 미세하게 벗어나는 수학적 오차 보정
         return Mathf.Clamp(normalizedNoise, 0f, 1f);
     }
